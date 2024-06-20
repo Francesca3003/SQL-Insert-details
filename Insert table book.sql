e798f661765a3c611d8e778d38d9a6a05fcafbaa
Francesca3003 <160511339+Francesca3003@users.noreply.github.com>
2024-06-20 10:12:43 +0200

    Add files via upload

---
+++ Insert table book.sql
@@ -0,0 +1,17 @@
+
+
+
+
+
+INSERT INTO book (titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count, editore, numero_pagine)
+VALUES ('Viaggio al centro', 'Jules Verne', 'Avventura', 1864, '1234567890123', 12.99, 4.5, 10, 'Verne Publishing', 350);
+
+INSERT INTO book (titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count, editore, numero_pagine)
+VALUES ('Guerra e pace', 'Lev Tolstoj', 'storico', 1869, '1234567890124', 14.99, 4.8, 5, 'Tolstoj Stampe', 1200);
+
+INSERT INTO book(titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count, editore, numero_pagine)
+VALUES ('I sussurri del vento', 'Amelia Blackburn', 'romantico', 1982, '1234567890125', 9.99, 4.2, 20, 'Blackburn House', 275);
+
+INSERT INTO book (titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count, editore, numero_pagine)
+VALUES ('L''Odissea Galattica', 'Orion Starfield', 'fantascienza', 2005, '1234567890126', 19.99, 4.9, 15, 'Nebulosa Press', 450);
+
